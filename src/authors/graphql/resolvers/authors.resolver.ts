6cd2bd3a1211465e78fd86ea8f346d70eb997bfa
Dinis <scorpaust@hotmail.com>
2024-01-25 23:35:52 +0000

    Config AuthorsPrismaRepository in AuthorsModule

---
+++ src/authors/graphql/resolvers/authors.resolver.ts
@@ -0,0 +1,15 @@
+import { Query, Resolver } from '@nestjs/graphql';
+import { PrismaService } from '../../../database/prisma/prisma.service';
+import { Author } from '../models/author';
+
+@Resolver(() => Author)
+export class AuthorsResolver {
+
+  constructor(private prisma: PrismaService) {}
+
+  @Query(() => [Author])
+  authors() {
+    return this.prisma.author.findMany()
+  }
+
+}
